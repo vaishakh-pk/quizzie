51f0f5e766fc43304732d9cf4ad5597b551d4687
VK <vaishakh.aravind@gmail.com>
2024-02-11 17:21:47 +0530

    display data and shuffle items

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizzie/styles/answer_button.dart';
+import 'package:quizzie/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -9,20 +10,23 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+
+  final currentQuestion = questions[0];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SizedBox(
       width: double.infinity,
       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const Text(
-          'The question',
-          style: TextStyle(color: Colors.white),
+         Text(
+          currentQuestion.text,
+          style: const TextStyle(color: Colors.white),
         ),
         const SizedBox(
           height: 30,
         ),
-        AnswerButton()
+        AnswerButton(answerText:currentQuestion.answers[0],onTap: (){})
       ]),
     ));
   }
