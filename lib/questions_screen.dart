73ef3975944f29be17ae1ef3551f0a6174df2137
VK <vaishakh.aravind@gmail.com>
2024-02-11 16:27:55 +0530

    ques page ui

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzie/styles/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,6 +12,18 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Questions Screen'));
+        body: SizedBox(
+      width: double.infinity,
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        const Text(
+          'The question',
+          style: TextStyle(color: Colors.white),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        AnswerButton()
+      ]),
+    ));
   }
 }
---
+++ lib/styles/answer_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(),
+            child: const Text('Options'));
+  }
+}
