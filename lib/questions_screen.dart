3bef7c4f3cc1e9ea811bd5c8644d5dfafc804f8e
VK <vaishakh.aravind@gmail.com>
2024-02-17 23:27:07 +0530

    google_fonts

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quizzie/styles/answer_button.dart';
 import 'package:quizzie/data/questions.dart';
 
@@ -37,7 +38,8 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
            Text(
             currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
+            style: GoogleFonts.lato(color: const Color.fromARGB(255, 201,153,251),fontSize: 24,
+            fontWeight: FontWeight.bold),
             textAlign: TextAlign.center,
           ),
           const SizedBox(
--- lib/start.dart
+++ lib/start.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Startpage extends StatelessWidget {
   Startpage(this.startQuiz,{super.key});
@@ -21,9 +22,9 @@ class Startpage extends StatelessWidget {
         const SizedBox(
           height: 20,
         ),
-        const Text(
+        Text(
           'Learn Flutter the fun way!',
-          style: TextStyle(fontSize: 20, color: Colors.white),
+          style: GoogleFonts.lato(fontSize: 20, color: Colors.white),
         ),
         const SizedBox(height:30),
         ElevatedButton.icon(onPressed: startQuiz ,icon: const Icon(Icons.arrow_right_alt), label: const Text('Start Quiz'))
